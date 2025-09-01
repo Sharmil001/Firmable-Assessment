546748fed6835eaeeeef6a31f60164cd96410d8b
Sharmil001 <sharmiladroja17@gmail.com>
2025-09-01 18:46:27 +0530

    updated file name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import CompanyDetails from "@/components/dashboard/company-lists";
+import CompanyLists from "@/components/dashboard/company-lists";
 import StatsCards from "@/components/dashboard/stats-cards";
 
 export default function Home() {
 	return (
 		<div className="container mx-auto mb-4 px-6">
 			<StatsCards />
-			<CompanyDetails />
+			<CompanyLists />
 		</div>
 	);
 }
